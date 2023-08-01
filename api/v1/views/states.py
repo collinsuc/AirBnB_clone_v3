dd032b4b892e1857cc1b70f538f128b7e76f10ff
collinsuc <collins.ahuchaogu@gmail.com>
2023-08-01 12:42:25 -0700

    l

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,72 +1,57 @@
 #!/usr/bin/python3
-'''Contains the states view for the API.'''
-from flask import jsonify, request
-from werkzeug.exceptions import NotFound, BadRequest
-
+"""
+Flask route that returns json status response
+"""
 from api.v1.views import app_views
-from models import storage
-from models.state import State
+from flask import abort, jsonify, make_response, request
+from flasgger import Swagger, swag_from
+from models import storage, CNC
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
-def get_states():
-    '''Get all states'''
-    query = storage.all(State).values()
-    states = list(map(lambda x: x.to_dict(), query))
-    return jsonify(states)
+@app_views.route('/states', methods=['GET', 'POST'])
+@swag_from('swagger_yaml/states_no_id.yml', methods=['GET', 'POST'])
+def states_no_id():
+    """
+        states route to handle http method for requested states no id provided
+    """
+    if request.method == 'GET':
+        all_states = storage.all('State')
+        all_states = list(obj.to_json() for obj in all_states.values())
+        return jsonify(all_states)
+
+    if request.method == 'POST':
+        req_json = request.get_json()
+        if req_json is None:
+            abort(400, 'Not a JSON')
+        if req_json.get("name") is None:
+            abort(400, 'Missing name')
+        State = CNC.get("State")
+        new_object = State(**req_json)
+        new_object.save()
+        return jsonify(new_object.to_json()), 201
 
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-def get_state(state_id=None):
-    '''Gets the state base on id'''
-    query = storage.all(State).values()
-    if state_id:
-        state = list(filter(lambda x: x.id == state_id, query))
-        if state:
-            return jsonify(state[0].to_dict())
-        raise NotFound()
+@app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'])
+@swag_from('swagger_yaml/states_id.yml', methods=['PUT', 'GET', 'DELETE'])
+def states_with_id(state_id=None):
+    """
+        states route to handle http method for requested state by id
+    """
+    state_obj = storage.get('State', state_id)
+    if state_obj is None:
+        abort(404, 'Not found')
 
+    if request.method == 'GET':
+        return jsonify(state_obj.to_json())
 
-@app_views.route('/states/<state_id>', methods=['DELETE'],
-                 strict_slashes=False)
-def delete_state(state_id=None):
-    '''delete a state with the given id.'''
-    states = storage.all(State).values()
-    query = list(filter(lambda x: x.id == state_id, states))
-    if query:
-        storage.delete(query[0])
-        storage.save()
-        return jsonify({}), 200
-    raise NotFound()
+    if request.method == 'DELETE':
+        state_obj.delete()
+        del state_obj
+        return jsonify({})
 
-
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
-def create_state(state_id=None):
-    '''creat a new state.'''
-    req = request.get_json()
-    if type(req) is not dict:
-        raise BadRequest(description='Not a JSON')
-    if 'name' not in req:
-        raise BadRequest(description='Missing name')
-    new_state = State(**req)
-    new_state.save()
-    return jsonify(new_state.to_dict()), 201
-
-
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-def update_state(state_id=None):
-    '''Updates the state base on an id.'''
-    ignore = ('id', 'created_at', 'updated_at')
-    query = storage.all(State).values()
-    result = list(filter(lambda x: x.id == state_id, query))
-    if result:
-        req = request.get_json()
-        if type(req) is not dict:
-            raise BadRequest(description='Not a JSON')
-        states = result[0]
-        for key, value in req.items():
-            if key not in ignore:
-                setattr(states, key, value)
-        states.save()
-        return jsonify(states.to_dict()), 200
-    raise NotFound()
+    if request.method == 'PUT':
+        req_json = request.get_json()
+        if req_json is None:
+            abort(400, 'Not a JSON')
+        state_obj.bm_update(req_json)
+        return jsonify(state_obj.to_json())
--- models/user.py
+++ models/user.py
@@ -1,49 +1,49 @@
 #!/usr/bin/python3
-""" holds class User"""
+"""
+User Class from Models Module
+"""
 import hashlib
-import models
+import os
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Float
+STORAGE_TYPE = os.environ.get('HBNB_TYPE_STORAGE')
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
-    if models.storage_t == 'db':
+    """
+        User class handles all application users
+    """
+    if STORAGE_TYPE == "db":
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship(
-            "Place",
-            cascade="all, delete, delete-orphan",
-            backref="user"
-        )
-        reviews = relationship(
-            "Review",
-            cascade="all, delete, delete-orphan",
-            backref="user"
-        )
+
+        places = relationship('Place', backref='user', cascade='delete')
+        reviews = relationship('Review', backref='user', cascade='delete')
     else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
 
     def __init__(self, *args, **kwargs):
-        """initializes user"""
+        """
+            instantiates user object
+        """
+        if kwargs:
+            pwd = kwargs.pop('password', None)
+            if pwd:
+                User.__set_password(self, pwd)
         super().__init__(*args, **kwargs)
 
-    def __setattr__(self, __name: str, __value) -> None:
-        '''Sets an attribute of this class to a given value.'''
-        if __name == 'password':
-            if type(__value) is str:
-                """Hash password using md5"""
-                m = hashlib.md5(bytes(__value, 'utf-8'))
-                super().__setattr__(__name, m.hexdigest())
-        else:
-            super().__setattr__(__name, __value)
-
+    def __set_password(self, pwd):
+        """
+            custom setter: encrypts password to MD5
+        """
+        secure = hashlib.md5()
+        secure.update(pwd.encode("utf-8"))
+        secure_password = secure.hexdigest()
+        setattr(self, "password", secure_password)
